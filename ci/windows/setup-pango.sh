cdf1513f9c2a90193ece49c390f21155f4ce7125
Norihiro Kamae <norihiro@nagater.net>
2021-05-28 18:30:16 +0900

    ci(windows): show details to run setup-pango

--- ci/windows/setup-pango.sh
+++ ci/windows/setup-pango.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
+set -x
 curl http://repo.msys2.org/msys/x86_64/zstd-1.4.5-2-x86_64.pkg.tar.xz | tar CvxJf / -
 curl http://repo.msys2.org/msys/x86_64/rsync-3.2.2-2-x86_64.pkg.tar.zst | tar Cxvf / - --zstd
 curl http://repo.msys2.org/msys/x86_64/openbsd-netcat-1.206_1-1-x86_64.pkg.tar.xz | tar CxvJf / -
 curl http://repo.msys2.org/msys/x86_64/libzstd-1.4.5-2-x86_64.pkg.tar.xz | tar CxvJf / -
 curl http://repo.msys2.org/msys/x86_64/libxxhash-0.8.0-1-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 
-./prepare.sh
+. ./prepare.sh
