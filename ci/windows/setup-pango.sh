4577fafcccdd84590368460cc742d1a24ed73a93
Norihiro Kamae <norihiro@nagater.net>
2021-05-28 18:48:40 +0900

    ci(windows): libpcre2_8-10.36-1-x86_64.pkg.tar.zst

--- ci/windows/setup-pango.sh
+++ ci/windows/setup-pango.sh
@@ -9,5 +9,6 @@ curl http://repo.msys2.org/msys/x86_64/openbsd-netcat-1.206_1-1-x86_64.pkg.tar.x
 curl http://repo.msys2.org/msys/x86_64/libzstd-1.4.5-2-x86_64.pkg.tar.xz | tar CxvJf / -
 curl http://repo.msys2.org/msys/x86_64/libxxhash-0.8.0-1-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 curl http://repo.msys2.org/msys/x86_64/wget-1.21.1-2-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
+curl http://repo.msys2.org/msys/x86_64/libpcre2_8-10.36-1-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 
 . ./prepare.sh
