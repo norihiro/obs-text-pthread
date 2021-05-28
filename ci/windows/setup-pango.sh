4f0d8f7c477fa93932f5d799cfe854276468b407
Norihiro Kamae <norihiro@nagater.net>
2021-05-28 19:07:04 +0900

    ci(windows): ninja and libgpgme

--- ci/windows/setup-pango.sh
+++ ci/windows/setup-pango.sh
@@ -11,5 +11,7 @@ curl http://repo.msys2.org/msys/x86_64/libxxhash-0.8.0-1-x86_64.pkg.tar.zst | ta
 curl http://repo.msys2.org/msys/x86_64/wget-1.21.1-2-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 curl http://repo.msys2.org/msys/x86_64/libpcre2_8-10.36-1-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 curl http://repo.msys2.org/msys/x86_64/libmetalink-0.1.3-3-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
+curl http://repo.msys2.org/msys/x86_64/ninja-1.10.2-1-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
+curl http://repo.msys2.org/msys/x86_64/libgpgme-1.15.1-2-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 
 . ./prepare.sh
