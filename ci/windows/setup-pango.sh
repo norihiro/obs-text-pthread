2323f565d72d622b3f523991863a1089d7396c5f
Norihiro Kamae <norihiro@nagater.net>
2021-05-28 18:41:07 +0900

    ci(windows): wget from HTTP server

--- ci/windows/setup-pango.sh
+++ ci/windows/setup-pango.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
 set -x
+env # check PATH, etc.
+
 curl http://repo.msys2.org/msys/x86_64/zstd-1.4.5-2-x86_64.pkg.tar.xz | tar CvxJf / -
 curl http://repo.msys2.org/msys/x86_64/rsync-3.2.2-2-x86_64.pkg.tar.zst | tar Cxvf / - --zstd
 curl http://repo.msys2.org/msys/x86_64/openbsd-netcat-1.206_1-1-x86_64.pkg.tar.xz | tar CxvJf / -
 curl http://repo.msys2.org/msys/x86_64/libzstd-1.4.5-2-x86_64.pkg.tar.xz | tar CxvJf / -
 curl http://repo.msys2.org/msys/x86_64/libxxhash-0.8.0-1-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
-
-mingw-get install msys-wget
+curl http://repo.msys2.org/msys/x86_64/wget-1.21.1-2-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 
 . ./prepare.sh
