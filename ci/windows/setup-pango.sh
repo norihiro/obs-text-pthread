88fa4ec28d5e68db9347af3676c8e4cd8b06e48b
Norihiro Kamae <norihiro@nagater.net>
2021-05-28 20:33:21 +0900

    ci(windows): ninja from github

--- ci/windows/setup-pango.sh
+++ ci/windows/setup-pango.sh
@@ -11,13 +11,15 @@ curl http://repo.msys2.org/msys/x86_64/libxxhash-0.8.0-1-x86_64.pkg.tar.zst | ta
 curl http://repo.msys2.org/msys/x86_64/wget-1.21.1-2-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 curl http://repo.msys2.org/msys/x86_64/libpcre2_8-10.36-1-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 curl http://repo.msys2.org/msys/x86_64/libmetalink-0.1.3-3-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
-curl http://repo.msys2.org/msys/x86_64/ninja-1.10.2-1-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
 curl http://repo.msys2.org/msys/x86_64/libgpgme-1.15.1-2-x86_64.pkg.tar.zst | tar Cvxf / - --zstd
+mkdir ninja
+curl -LO https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-win.zip && unzip ninja-win.zip -d ninja
+PATH="$PWD/ninja:$PATH"
 
 # -G 'Ninja'
 # -G "Visual Studio 16 2019"
 sed -i \
 	-e "s;-G 'Ninja';-G 'Visual Studio 16 2019';g" \
-	-e 's/ninja install/cmake --config Release --build . && cmake --config Release --install ./g' \
+	-e 's/ninja install$/cmake --config Release --build . ; cmake --config Release --install ./g' \
 	./prepare.sh
 . ./prepare.sh
