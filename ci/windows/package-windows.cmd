6cdd9b20fd5239e8775ebe68d7fe2bd8a5da662f
Norihiro Kamae <norihiro@nagater.net>
2025-08-29 11:33:11 +0900

    Add ci/windows/package-windows.cmd

---
+++ ci/windows/package-windows.cmd
@@ -0,0 +1,20 @@
+call "build\ci\ci_includes.generated.cmd"
+
+mkdir package
+cd package
+
+git describe --tags --always > package-version.txt
+set /p PackageVersion=<package-version.txt
+del package-version.txt
+
+copy ..\LICENSE          ..\release\data\obs-plugins\%PluginName%\LICENCE-%PluginName%.txt
+copy ..\deps\libebur128\COPYING ..\release\data\obs-plugins\%PluginName%\LICENCE-libebur128.txt
+
+REM Package ZIP archive
+7z a "%PluginName%-%PackageVersion%-obs%1-Windows.zip" "..\release\*"
+
+REM Build installer
+iscc ..\build\installer-Windows.generated.iss /O. /F"%PluginName%-%PackageVersion%-obs%1-Windows-Installer"
+
+certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows.zip" SHA1
+certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows-Installer.exe" SHA1
